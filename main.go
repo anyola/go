df4dbe98895f7ad6c7c2f9553bea81ff8857b978
Alyona-slukinova <slukinova.alyona@gmail.com>
2026-09-17 16:36:18 +0300

    add twoSum

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
 import "fmt"
+func twoSum(nums []int, target int) []int {
+    m := make(map[int]int)
+    for i := 0; i < len(nums); i++ {
+        var dif int = target - nums[i];
+        if j, ok := m[dif]; ok {
+            return []int{j, i}
+        }
+        m[nums[i]] = i
+    }
+    return []int{}
+}
 
 func main() {
-    fmt.Println("Hello, Go!")
+    nums := []int{1, 2, 3, 5}
+
+	result := twoSum(nums, 5)
+
+	fmt.Println(result)
 }
