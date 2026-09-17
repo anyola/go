136aa3ae86f48f060e16b44e1bd30bd47ba2e07b
Alyona-slukinova <slukinova.alyona@gmail.com>
2026-09-17 18:15:26 +0300

    add formatting in main.go

--- main.go
+++ main.go
@@ -1,20 +1,21 @@
 package main
 
 import "fmt"
+
 func twoSum(nums []int, target int) []int {
-    m := make(map[int]int)
-    for i := 0; i < len(nums); i++ {
-        var dif int = target - nums[i];
-        if j, ok := m[dif]; ok {
-            return []int{j, i}
-        }
-        m[nums[i]] = i
-    }
-    return []int{}
+	m := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		var dif int = target - nums[i]
+		if j, ok := m[dif]; ok {
+			return []int{j, i}
+		}
+		m[nums[i]] = i
+	}
+	return []int{}
 }
 
 func main() {
-    nums := []int{1, 2, 3, 5}
+	nums := []int{1, 2, 3, 5}
 
 	result := twoSum(nums, 5)
 
